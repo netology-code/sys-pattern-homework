a45b7d491702f7b201ecb5623d674dec96c61b99
Aleksandr Shubin <aleksandr.shubin.255@gmail.com>
2023-02-10 17:32:38 +0300

    Изменения, которые будут включены в коммит:

--- 11-04/producer.py
+++ 11-04/producer.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.56.11'))
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 channel.queue_declare(queue='hello')
 channel.basic_publish(exchange='', routing_key='hello', body='Hello Netology!')
