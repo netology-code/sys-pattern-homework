a2cb26ace9c81e7a4915aa927f53fafabc90488c
Aleksandr Shubin <aleksandr.shubin.255@gmail.com>
2023-02-10 17:19:39 +0300

    Ваша ветка обновлена в соответствии с «origin/main».

--- 11-04/producer.py
+++ 11-04/producer.py
@@ -2,8 +2,8 @@
 # coding=utf-8
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.56.11'))
 channel = connection.channel()
-channel.queue_declare(queue='netology')
-channel.basic_publish(exchange='', routing_key='hello', body='Hello world!')
+channel.queue_declare(queue='hello')
+channel.basic_publish(exchange='', routing_key='hello', body='Hello Netology!')
 connection.close()
