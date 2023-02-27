b7bb99b4db5e279473fb88fa3e85ff04f67168ac
Aleksandr Shubin <aleksandr.shubin.255@gmail.com>
2023-02-27 17:07:09 +0300

    Изменения, которые будут включены в коммит:

--- virt/05-virt-04/terraform/.terraform.lock.hcl
+++
@@ -1,9 +0,0 @@
-# This file is maintained automatically by "terraform init".
-# Manual edits may be lost in future updates.
-
-provider "registry.terraform.io/yandex-cloud/yandex" {
-  version = "0.85.0"
-  hashes = [
-    "h1:3MBmptadpBzcbREAXEzratBMXPcZk1GZMEjERg+JVJg=",
-  ]
-}
---
+++ virt/05-virt-05/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gkr8i19c9ih1i3add6"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1g2dj02nhisso9oaoqn"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8s2aq8skr5t6vrujfo"
+}
