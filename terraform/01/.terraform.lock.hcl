37df80800a5e33054a3097eecc2a1fb74371ecb0
Aleksandr Shubin <aleksandr.shubin.255@gmail.com>
2023-04-04 11:49:08 +0300

    Ваша ветка обновлена в соответствии с «origin/main».

---
+++ terraform/01/.terraform.lock.hcl
@@ -0,0 +1,10 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/shekeriev/virtualbox" {
+  version     = "0.0.4"
+  constraints = "0.0.4"
+  hashes = [
+    "h1:mxbItWbB7QtqVgaaDJUQymGm8pmF3IexznNdUe2EeLU=",
+  ]
+}
---
+++ terraform/01/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    virtualbox = {
+      source = "shekeriev/virtualbox"
+      version = "0.0.4"
+    }
+  }
+}
+
+provider "virtualbox" {
+  delay      = 60
+  mintimeout = 5
+}
+
+resource "virtualbox_vm" "vm3" {
+  name   = "debian-11"
+  image  = "https://app.vagrantup.com/shekeriev/boxes/debian-11/versions/0.2/providers/virtualbox.box"
+  cpus      = 1
+  memory    = "512 mib"
+
+  network_adapter {
+    type           = "hostonly"
+    device         = "IntelPro1000MTDesktop"
+    host_interface = "vboxnet0"
+    # On Windows use this instead
+    # host_interface = "VirtualBox Host-Only Ethernet Adapter"
+  }
+}
+
+output "IPAddress" {
+  value = element(virtualbox_vm.vm3.*.network_adapter.0.ipv4_address, 1)
+}
