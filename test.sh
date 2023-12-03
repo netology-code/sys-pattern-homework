c686e1639135e6fef1da6cc228d780b89bf85b01
SoReX48 <insobj8@gmail.com>
2023-12-03 18:54:23 +0300

    Fifth commit

--- test.sh
+++ test.sh
@@ -12,3 +12,10 @@ SdSDSDl,dfagb,lsfg,lfagldfgasd
 	asdlaslfsdlfasldflaflsadlfasldfalfalf
   }
 }}
+
+sdasdasdsadafgb.sfg;nb;gfnb
+safdasdg.sdf;bg.s;dfbs;dfb.;sfd
+sdfsd;gvs.;bs;db.dsf
+
+*****sfsdalfgalsdg,las,galsdg*****
+			dfdf	df	dfd+
